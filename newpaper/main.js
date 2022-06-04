b281a33847c351ecc2a4f836da3861aa4c21da50
ornn <info@ornn.co.kr>
2022-06-04 11:32:53 +0900

    no message

--- newpaper/main.js
+++ newpaper/main.js
@@ -1,4 +1,6 @@
 let news = [];
+let page = 1;
+let total_pages = 0;
 let menus = document.querySelectorAll('.menus button');
 let searchButton = document.getElementById("search-button");
 let url; // 전역변수로 만든다.
@@ -11,6 +13,8 @@ api호출 함수를 부른다
 const getNews = async ()=> {
   try {
     let header = new Headers({'x-api-key' : '6hFzs__u0LySFevSajqafqSwoFNBh4GmSZNfZPlEfC4'});
+    url.searchParams.set('page', page); // 쿼리에 page 추가
+
     let response = await fetch(url, {headers : header});
     let data = await response.json(); // data type json으로 변경
     if (response.status == 200) {
@@ -18,7 +22,10 @@ const getNews = async ()=> {
         throw new Error("검색된 결과값이 없습니다.");
       }
       news = data.articles;
+      total_pages = data.total_pages;
+      page = data.page;
       render();
+      pagenation();
     } else {
       throw new Error(data.message);
     }
@@ -76,6 +83,42 @@ const render = ()=> {
 const errorRender = (message)=> {
   let errorHTML = `<div class="alert alert-danger text-center" role="alert">${message}</div>`;
   document.getElementById("news-board").innerHTML = errorHTML;
+}
+
+const pagenation = ()=> {
+  let pagenationHTML = ``;
+  let pageGroup = Math.ceil(page / 5);
+  let last = pageGroup * 5;
+  let first = last - 4;
+
+  // total page 3개일 경우 3개의 페이지만 프린트 하는 방법
+  // << >> 맨 처음과 맨 끝 페이지로 이동하는 버튼
+  // 내가 그룹1 일때 << < 이 버튼이 없다.
+  // 내가 마지막 그룹일때 >> > 이 버튼이 없다.
+
+  pagenationHTML = `<li class="page-item">
+      <a class="page-link" href="#" aria-label="Previous" onClick="moveToPage(${page-1})">
+        <span aria-hidden="true">&lt;</span>
+      </a>
+    </li>`;
+
+  for (let i = first; i <= last; i++) {
+    pagenationHTML += `<li class="page-item ${page==i?"active":""}"><a class="page-link" href="#" onClick="moveToPage(${i})">${i}</a></li>`;
+  }
+
+  pagenationHTML += `<li class="page-item">
+      <a class="page-link" href="#" aria-label="Next" onClick="moveToPage(${page+1})">
+        <span aria-hidden="true">&gt;</span>
+      </a>
+    </li>`;
+  
+
+  document.querySelector(".pagination").innerHTML = pagenationHTML;
+}
+
+const moveToPage = (pageNum)=> {
+  page = pageNum;
+  getNews();
 }
 
 // 검색 : 호이스팅 문제를 해결하기 위해 하단에 위치한다.
